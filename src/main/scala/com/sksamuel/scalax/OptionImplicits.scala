7bc61c1ef4235e19a8e3b5e1c18457188bc0cb52
sksamuel <sam@sksamuel.com>
2016-01-27 07:22:58 +0000

    Added non empty string option

--- src/main/scala/com/sksamuel/scalax/OptionImplicits.scala
+++ src/main/scala/com/sksamuel/scalax/OptionImplicits.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.scalax
 
-object Options {
+object OptionImplicits {
 
   /**
     * Better than Some(t) because that will return the inferred type as Some[T], but in a fold we probably want the
@@ -9,4 +9,8 @@ object Options {
   implicit def some[T](t: T): Option[T] = Some(t)
 
   def none[T]: Option[T] = None
+}
+
+object NonEmptyString {
+  def apply(str: String): Option[String] = Option(str).filter(_.trim.nonEmpty)
 }
