0d8f2fe87bd4762ed221d04fa19bb261ea1b9ae7
sksamuel <sam@sksamuel.com>
2016-02-14 10:38:43 +0000

    Added test that work is shared in bqci

--- src/test/scala/com/sksamuel/scalax/collection/BlockingQueueConcurrentIteratorTest.scala
+++ src/test/scala/com/sksamuel/scalax/collection/BlockingQueueConcurrentIteratorTest.scala
@@ -13,15 +13,33 @@ class BlockingQueueConcurrentIteratorTest extends WordSpec with Matchers {
 
   "BlockingQueueConcurrentIterator" should {
     "provide multiple thread-safe iterators" in {
+
       val queue = new LinkedBlockingQueue[Int]
       Iterator.range(0, 10000).foreach(_ => queue.put(1))
       queue.put(-1)
+
       val futures = for ( k <- 1 to 10 ) yield Future {
         BlockingQueueConcurrentIterator(queue, -1).size
       }
+
       val f = Future.sequence(futures)
       val total = Await.result(f, 10.seconds).sum
       total shouldBe 10000
+    }
+    "share work between multiple iterators" in {
+
+      val queue = new LinkedBlockingQueue[Int]
+      Iterator.range(0, 100000).foreach(_ => queue.put(1))
+      queue.put(-1)
+
+      val futures = for ( k <- 1 to 10 ) yield Future {
+        BlockingQueueConcurrentIterator(queue, -1).size
+      }
+
+      val f = Future.sequence(futures)
+      val results = Await.result(f, 10.seconds)
+      // work should have been shared
+      results.count(_ >= 100) > 2
     }
   }
 }
