6c2df2500368779b00b33945fb9f7c7a56517ed8
sksamuel <sam@sksamuel.com>
2016-02-21 15:50:51 +0000

    Fixing bug parsing ? as empty map

--- src/test/scala/com/sksamuel/scalax/net/UrlParamParserTest.scala
+++ src/test/scala/com/sksamuel/scalax/net/UrlParamParserTest.scala
@@ -8,6 +8,9 @@ class UrlParamParserTest extends WordSpec with Matchers {
     "parse params" in {
       UrlParamParser("?a=b&c=d") shouldBe Map("a" -> List("b"), "c" -> List("d"))
     }
+    "parse single ? as empty map" in {
+      UrlParamParser("?") shouldBe Map.empty
+    }
     "support multi params" in {
       UrlParamParser("a=b&c=d&a=z") shouldBe Map("a" -> List("b", "z"), "c" -> List("d"))
     }
