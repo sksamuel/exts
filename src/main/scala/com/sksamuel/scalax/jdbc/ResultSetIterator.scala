448d919254edb9037bbfd610150d83e650bbd3bc
sksamuel <sam@sksamuel.com>
2016-05-06 10:04:43 +0100

    Added resultset iterator non strings method

--- src/main/scala/com/sksamuel/scalax/jdbc/ResultSetIterator.scala
+++ src/main/scala/com/sksamuel/scalax/jdbc/ResultSetIterator.scala
@@ -3,10 +3,16 @@ package com.sksamuel.scalax.jdbc
 import java.sql.ResultSet
 
 object ResultSetIterator {
-  def apply(rs: ResultSet): Iterator[Array[String]] = new Iterator[Array[String]] {
+
+  def strings(rs: ResultSet): Iterator[Array[String]] = new Iterator[Array[String]] {
     override def hasNext: Boolean = rs.next()
     override def next(): Array[String] = {
       for ( k <- 1 to rs.getMetaData.getColumnCount ) yield rs.getString(k)
     }.toArray
+  }
+
+  def apply(rs: ResultSet): Iterator[ResultSet] = new Iterator[ResultSet] {
+    override def hasNext: Boolean = rs.next()
+    override def next(): ResultSet = rs
   }
 }
