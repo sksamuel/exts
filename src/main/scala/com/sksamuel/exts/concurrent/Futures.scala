45fe1b7b8d24df528c783007eb1adb2fd23860fa
sksamuel <sam@sksamuel.com>
2016-07-24 12:43:46 +0100

    Added a fail fast future

--- src/main/scala/com/sksamuel/exts/concurrent/Futures.scala
+++ src/main/scala/com/sksamuel/exts/concurrent/Futures.scala
@@ -1,9 +1,32 @@
 package com.sksamuel.exts.concurrent
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import scala.concurrent.{ExecutionContext, Future, Promise}
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Success}
 
 object Futures {
+
+  /**
+    * Returns the error from the first future that fails, without waiting for the rest of the futures to complete.
+    * If all futures complete successfully then the returned Future contains unit
+    * If a future fails then the returned return will be marked as failure as well, with the original
+    * exception set on the returned future.
+    */
+  def failFast[T](futures: Iterable[Future[T]])(implicit ex: ExecutionContext): Future[Unit] = {
+    val promise = Promise[Unit]
+    val k = new AtomicInteger(futures.size)
+    futures.foreach { future =>
+      future.onComplete {
+        case Success(_) =>
+          if (k.decrementAndGet == 0)
+            promise.success(())
+        case Failure(e) =>
+          promise.failure(e)
+      }
+    }
+    promise.future
+  }
 
   def firstThrowableOf[T](futures: TraversableOnce[Future[T]])(implicit ex: ExecutionContext): Future[Option[Throwable]] = {
     // this turns all futures into futures of trys from which we can get the first failure
