10b6af17d09224f9b0196cbb36db11a204135c11
sksamuel <sam@sksamuel.com>
2016-07-24 12:30:30 +0100

    Add findOrElse #4

--- src/main/scala/com/sksamuel/scalax/collection/Collections.scala
+++ src/main/scala/com/sksamuel/scalax/collection/Collections.scala
@@ -5,6 +5,9 @@ import scala.collection.mutable
 object Collections {
 
   implicit class RichSequence[T](seq: Seq[T]) {
+
+    def findOrElse[U >: T](p: T => Boolean)(default: U): U = seq.find(p).getOrElse(default)
+
     def distinctBy[U](f: T => U): Seq[T] = {
       val seen = mutable.HashSet[U]()
       seq.filter { t =>
