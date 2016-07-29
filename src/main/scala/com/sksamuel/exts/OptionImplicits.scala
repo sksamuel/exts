efdfa0c612aa8c41c0d743efd47a9f21bf0c33df
sksamuel <sam@sksamuel.com>
2016-07-29 21:07:17 +0100

    Updated the way the implicit options work

--- src/main/scala/com/sksamuel/exts/OptionImplicits.scala
+++ src/main/scala/com/sksamuel/exts/OptionImplicits.scala
@@ -8,7 +8,13 @@ object OptionImplicits {
     * Better than Some(t) because that will return the inferred type as Some[T], but in a fold we probably want the
     * type inferred as Option[T]
     */
-  implicit def some[T](t: T): Option[T] = Some(t)
+  implicit class RichOptionImplicits[T](t: T) {
+    def some: Option[T] = Some(t)
+  }
+
+  implicit class RichStringOptionImplicits(str: String) {
+    def some: Option[String] = StringOption(str)
+  }
 
   def none[T]: Option[T] = None
 }
