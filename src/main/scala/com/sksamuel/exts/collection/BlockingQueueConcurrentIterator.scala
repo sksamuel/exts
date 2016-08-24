74638db32d833cecbc5b33e5bca6079bef2f12b2
sksamuel <sam@sksamuel.com>
2016-08-24 20:58:38 +0100

    Updated some docs

--- src/main/scala/com/sksamuel/exts/collection/BlockingQueueConcurrentIterator.scala
+++ src/main/scala/com/sksamuel/exts/collection/BlockingQueueConcurrentIterator.scala
@@ -2,6 +2,11 @@ package com.sksamuel.exts.collection
 
 import java.util.concurrent.BlockingQueue
 
+// Accepts a blocking queue and a sentinel value, and returns a Scala iterator.
+// Allows multiple iterators to be created on the same blocking queue, hence the ConcurrentIterator name.
+// The  iterator will return elements until it encounters the sentinel value. After finding the sentinel
+// the iterator will keep the sentinel on the queue so that any other iterators on the same source
+// can also detect it and terminate.
 case class BlockingQueueConcurrentIterator[E](queue: BlockingQueue[E], sentinel: E) extends Iterator[E] {
   private val iterator: Iterator[E] = {
     Iterator.continually(queue.take).takeWhile { e =>
--- src/main/scala/com/sksamuel/exts/collection/ConcurrentLinkedQueueConcurrentIterator.scala
+++ src/main/scala/com/sksamuel/exts/collection/ConcurrentLinkedQueueConcurrentIterator.scala
@@ -2,6 +2,8 @@ package com.sksamuel.exts.collection
 
 import java.util.concurrent.ConcurrentLinkedQueue
 
+// converts a java ConcurrentLinkedQueue into a scala Iterator.
+// the iterator continues to request elements from the queue until it finds a null
 case class ConcurrentLinkedQueueConcurrentIterator[E](queue: ConcurrentLinkedQueue[E]) extends Iterator[E] {
   private val iterator: Iterator[E] = Iterator.continually(queue.poll).takeWhile(_ != null)
   override def hasNext: Boolean = iterator.hasNext
