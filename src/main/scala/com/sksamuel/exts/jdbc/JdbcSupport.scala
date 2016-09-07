f04d42f93b318a7c0dd0b4bbd115f75687998d64
sksamuel <sam@sksamuel.com>
2016-09-07 22:48:45 +0100

    Updated jdbc to support user/password in connections

--- src/main/scala/com/sksamuel/exts/jdbc/JdbcSupport.scala
+++ src/main/scala/com/sksamuel/exts/jdbc/JdbcSupport.scala
@@ -1,6 +1,7 @@
 package com.sksamuel.exts.jdbc
 
 import java.sql.{Connection, DriverManager}
+import java.util.Properties
 
 import scala.util.control.NonFatal
 
@@ -8,10 +9,11 @@ trait JdbcSupport {
   outer =>
 
   def withConnection[T](url: String,
+                        properties: Map[String, AnyRef] = Map.empty,
                         autoCommit: Boolean = true,
                         schema: String = null,
                         catalog: String = null)(fn: Connection => T): T = {
-    val conn = openConnection(url, autoCommit, schema, catalog)
+    val conn = openConnection(url, properties, autoCommit, schema, catalog)
     try {
       fn(conn)
     } finally {
@@ -20,10 +22,13 @@ trait JdbcSupport {
   }
 
   def openConnection(url: String,
+                     properties: Map[String, AnyRef] = Map.empty,
                      autoCommit: Boolean = true,
                      schema: String = null,
                      catalog: String = null): Connection = {
-    val conn = DriverManager.getConnection(url)
+    val props = new Properties()
+    properties.foreach { case (key, value) => props.put(key, value) }
+    val conn = DriverManager.getConnection(url, props)
     conn.setAutoCommit(autoCommit)
     if (schema != null)
       conn.setSchema(schema)
