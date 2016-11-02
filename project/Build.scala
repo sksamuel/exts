4e92723a260f179ef3e77503e8ea0772efd8141b
sksamuel <sam@sksamuel.com>
2016-11-02 11:10:35 +0000

    Added 2.12.0 as default

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object Build extends Build {
 
   val org = "com.sksamuel.exts"
 
-  val ScalaVersion = "2.11.8"
+  val ScalaVersion = "2.12.0"
   val ScalatestVersion = "3.0.0"
   val Slf4jVersion = "1.7.12"
   val Log4jVersion = "1.2.17"
@@ -14,7 +14,7 @@ object Build extends Build {
   val rootSettings = Seq(
     organization := org,
     scalaVersion := ScalaVersion,
-    crossScalaVersions := Seq(ScalaVersion, "2.12.0-RC1"),
+    crossScalaVersions := Seq(ScalaVersion, "2.11.8"),
     publishMavenStyle := true,
     resolvers += Resolver.mavenLocal,
     publishArtifact in Test := false,
