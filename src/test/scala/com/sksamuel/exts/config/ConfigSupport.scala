fccb8e617dfe962a03381c29d66d361ca7fcc46c
sksamuel <sam@sksamuel.com>
2016-11-09 09:30:48 +0000

    Added config support

---
+++ src/test/scala/com/sksamuel/exts/config/ConfigSupport.scala
@@ -0,0 +1,17 @@
+package com.sksamuel.exts.config
+
+import com.typesafe.config.Config
+import scala.collection.JavaConverters._
+
+trait ConfigSupport {
+
+  implicit class RichConfig(config: Config) {
+    def getStringSeq(path: String): Seq[String] = if (config.hasPath(path)) config.getStringList(path).asScala else Nil
+    def getIntSeq(path: String): Seq[Int] = if (config.hasPath(path)) config.getIntList(path).asScala.map(_.toInt) else Nil
+    def getStringOrElse(path: String, or: String) = if (config.hasPath(path)) config.getString(path) else or
+    def getDoubleOrElse(path: String, or: String) = if (config.hasPath(path)) config.getDouble(path) else or
+    def getBooleanOrElse(path: String, or: String) = if (config.hasPath(path)) config.getBoolean(path) else or
+    def getIntOrElse(path: String, or: String) = if (config.hasPath(path)) config.getInt(path) else or
+    def getLongOrElse(path: String, or: String) = if (config.hasPath(path)) config.getLong(path) else or
+  }
+}
