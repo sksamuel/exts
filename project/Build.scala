9731ee20e221da3eb2f2f2f0200adb5bbf7c1dfe
sksamuel <sam@sksamuel.com>
2016-11-13 22:10:48 +0000

    Added 2.10 cross build

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object Build extends Build {
   val rootSettings = Seq(
     organization := org,
     scalaVersion := ScalaVersion,
-    crossScalaVersions := Seq(ScalaVersion, "2.11.8"),
+    crossScalaVersions := Seq(ScalaVersion, "2.11.8", "2.10.6"),
     publishMavenStyle := true,
     resolvers += Resolver.mavenLocal,
     publishArtifact in Test := false,
