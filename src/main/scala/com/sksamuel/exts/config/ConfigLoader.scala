c371d382c199ffada791b13d499f8dc579413431
sksamuel <sam@sksamuel.com>
2017-04-11 00:52:35 +0100

    Config loader now much configurable

--- src/main/scala/com/sksamuel/exts/config/ConfigLoader.scala
+++ src/main/scala/com/sksamuel/exts/config/ConfigLoader.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.exts.config
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 
 import com.sksamuel.exts.Logging
 import com.sksamuel.exts.OptionImplicits._
@@ -9,66 +9,119 @@ import com.typesafe.config.{Config, ConfigFactory}
 /**
   * Supports environment and application config in an opinionated way.
   *
-  * The application config is loaded from the name specified in the classpath.
-  * Eg if the app name is "foo" then the application conf is foo.conf and this must
-  * exist on the classpath at /foo.conf
+  * Starting from the lowest priority to the highest:
   *
-  * The environment config is loaded from a file called $env.conf where env is determined
-  * from a sys property called CONFIG_ENV. If no env is found, it will default to LOCAL.
-  * The $env.conf file will be searched for the following places, with the first match winning:
-  * - current working directory (cwd)
-  * - users home folder
-  * - classpath
+  * - all reference.confs are loaded from the classpath and merged; the order is not defined.
+  * - application.conf is loaded from the classpath
+  * - ENV.conf is loaded from working dir, user home, and classpath in that order, and merged.
+  * - override.conf is loaded from working dir and user home and merged.
   *
-  * Finally an override.conf can be located in the cwd only.
+  * Override.conf is designed to allow easy, temporary, overrides at runtime for a particular process,
+  * and therefore is only ever loaded from the user home and cwd.
   *
-  * The order of priority, highest at the top:
-  * ./override.conf
-  * ./${env}.conf or ~/${env}.conf or classpath:${env}.conf
-  * classpath:${application}.conf
-  * classpath:reference.conf (all reference.conf files on the classpath are merged into a single file)
+  * The env config is designed to change between environments. The filename has a placeholder $ENV which is
+  * replaced by the value of the system property called by default CONFIG_ENV.
+  * If no env is found, it will default to LOCAL.
+  *
   */
+case class ConfigLoaderParams(overrideConfInUserHome: Boolean = true,
+                              overrideConfInWorkingDir: Boolean = true,
+                              envConfInWorkingDir: Boolean = true,
+                              envConfInClasspath: Boolean = true,
+                              envConfInUserHome: Boolean = true,
+                              envConfFilename: String = "$ENV.conf",
+                              applicationConfName: String = "application.conf",
+                              configEnvParameterName: String = "CONFIG_ENV",
+                              overrideConfName: String = "override.conf")
+
 object ConfigLoader extends Logging {
 
   val ConfigKey = "CONFIG_ENV"
   val LocalEnv = "LOCAL"
 
+  def load(params: ConfigLoaderParams = ConfigLoaderParams()): Config = {
+
+    val env = Option(sys.props(ConfigKey)).orElse(sys.env.get(ConfigKey)).getOrElse(LocalEnv)
+    val resolvedEnvConfFilename = params.envConfFilename.replace("$ENV", env)
+    val userHome = System.getProperty("user.home")
+
+    def loadPath(path: Path): Config = {
+      path.toAbsolutePath.some.map { path =>
+        logger.info(s"Loading path:$path")
+        ConfigFactory.parseFile(path.toFile)
+      }.getOrElse {
+        ConfigFactory.empty()
+      }
+    }
+
+    def loadResource(resource: String): Config = {
+      logger.info(s"Loading classpath:$resource (if present)")
+      ConfigFactory.parseResources(resource)
+    }
+
+    def overrideConf(): Config = {
+      def loadOverrideFromDir(path: String) = loadPath(Paths.get(path).resolve(params.overrideConfName))
+      val workingDirConf = if (params.overrideConfInWorkingDir) loadOverrideFromDir(".") else ConfigFactory.empty()
+      val userHomeConf = if (params.overrideConfInUserHome) loadOverrideFromDir(userHome) else ConfigFactory.empty()
+      workingDirConf.withFallback(userHomeConf)
+    }
+
+    def envConf(): Config = {
+      def loadEnvConfFromDir(path: String) = loadPath(Paths.get(path).resolve(resolvedEnvConfFilename))
+      val workingDirConf = if (params.envConfInWorkingDir) loadEnvConfFromDir(".") else ConfigFactory.empty()
+      val userHomeConf = if (params.envConfInUserHome) loadEnvConfFromDir(userHome) else ConfigFactory.empty()
+      val classpathConf = loadResource(resolvedEnvConfFilename)
+      workingDirConf.withFallback(userHomeConf).withFallback(classpathConf)
+    }
+
+    val refconf = ConfigFactory.defaultReference()
+    val appconf = ConfigFactory.parseResources(params.applicationConfName)
+    val envconf = envConf()
+    val overrideconf = overrideConf()
+
+    overrideconf.withFallback(envconf).withFallback(appconf).withFallback(refconf).resolve()
+  }
+
+  @deprecated("use apply(params: ConfigLoaderParams)", "1.41.0")
   def apply(appName: String = "application"): Config = {
+
+    def locateAndLoadOverrideConf(name: String): Config = {
+      val overrideConfPath = Paths.get(".").resolve(name).toAbsolutePath
+      overrideConfPath.some.map {
+        path =>
+          logger.info(s"Loading OVERRIDE:$path")
+          ConfigFactory.parseFile(path.toFile)
+      }.getOrElse {
+        logger.info(s"No override conf located at $overrideConfPath")
+        ConfigFactory.empty()
+      }
+    }
+
+    def locateAndLoadEnvConf(env: String): Config = {
+      val confFilename = s"$env.conf"
+
+      // 1st place to check is the cwd
+      val cwdConfFile = Paths.get(".").resolve(confFilename).toAbsolutePath.some
+
+      // 2nd place to look is in the users home folder
+      val homeFolderConfFile = Paths.get(sys.props("user.home")).resolve(confFilename).toAbsolutePath.some
+
+      cwdConfFile.orElse(homeFolderConfFile).map {
+        path =>
+          logger.info(s"Loading ENV:$env config from $path")
+          ConfigFactory.parseFile(path.toFile)
+      }.getOrElse {
+        // 3rd place to look is the classpath
+        logger.info(s"Loading ENV:$env config from classpath (if present)")
+        ConfigFactory.parseResources(confFilename)
+      }
+    }
+
     val env = Option(sys.props(ConfigKey)).orElse(sys.env.get(ConfigKey)).getOrElse(LocalEnv)
     val refConf = ConfigFactory.defaultReference()
     val appConf = ConfigFactory.parseResources(s"$appName.conf")
     val envConf = locateAndLoadEnvConf(env)
-    val overConf = locateAndLoadOverrideConf()
+    val overConf = locateAndLoadOverrideConf("override.conf")
     overConf.withFallback(envConf).withFallback(appConf).withFallback(refConf).resolve()
-  }
-
-  def locateAndLoadOverrideConf(): Config = {
-    val overrideConfPath = Paths.get(".").resolve("override.conf").toAbsolutePath
-    overrideConfPath.some.map { path =>
-      logger.info(s"Loading OVERRIDE:$path")
-      ConfigFactory.parseFile(path.toFile)
-    }.getOrElse {
-      logger.info(s"No override conf located at $overrideConfPath")
-      ConfigFactory.empty()
-    }
-  }
-
-  def locateAndLoadEnvConf(env: String): Config = {
-    val confFilename = s"$env.conf"
-
-    // 1st place to check is the cwd
-    val cwdConfFile = Paths.get(".").resolve(confFilename).toAbsolutePath.some
-
-    // 2nd place to look is in the users home folder
-    val homeFolderConfFile = Paths.get(sys.props("user.home")).resolve(confFilename).toAbsolutePath.some
-
-    cwdConfFile.orElse(homeFolderConfFile).map { path =>
-      logger.info(s"Loading ENV:$env config from $path")
-      ConfigFactory.parseFile(path.toFile)
-    }.getOrElse {
-      // 3rd place to look is the classpath
-      logger.info(s"Loading ENV:$env config from classpath (if present)")
-      ConfigFactory.parseResources(confFilename)
-    }
   }
 }
