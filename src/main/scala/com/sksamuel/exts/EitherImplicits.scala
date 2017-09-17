e6daf37d07f5545adf103f1924f02cd46d81c563
sksamuel <sam@sksamuel.com>
2017-09-17 09:44:13 +0100

    Added rich either

--- src/main/scala/com/sksamuel/exts/EitherImplicits.scala
+++ src/main/scala/com/sksamuel/exts/EitherImplicits.scala
@@ -1,8 +1,13 @@
 package com.sksamuel.exts
 
 object EitherImplicits {
-  implicit class RichEitherImplicits[T](t: T) {
+
+  implicit class EitherOps[T](t: T) {
     def left[R]: Left[T, R] = Left(t)
     def right[L]: Right[L, T] = Right(t)
+  }
+
+  implicit class RichEither[L, R](either: Either[L, R]) {
+    def getOrError(message: String): R = either.right.getOrElse(sys.error(message))
   }
 }
