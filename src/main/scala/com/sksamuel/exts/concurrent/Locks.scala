d450c894b7cd9fec99e34ff9b51d06f47b6453bb
sksamuel <sam@sksamuel.com>
2018-12-18 09:14:47 +0000

    Added locks helper

---
+++ src/main/scala/com/sksamuel/exts/concurrent/Locks.scala
@@ -0,0 +1,14 @@
+package com.sksamuel.exts.concurrent
+
+import java.util.concurrent.locks.Lock
+
+trait Locks {
+  def withLockInterruptibly[T](lock: Lock)(thunk: => T): T = {
+    try {
+      lock.lockInterruptibly()
+      thunk
+    } finally {
+      lock.unlock()
+    }
+  }
+}
